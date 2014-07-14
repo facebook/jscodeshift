fcba018dc4825e35b5b6a5313f71965b8c01c7c7
Felix Kling <felix.kling@gmx.net>
2014-07-14 11:59:59 -0700

    Add support for arrow functions in the codebase

--- jest/transform.js
+++ jest/transform.js
@@ -2,8 +2,9 @@
 
 var jstransform = require('jstransform');
 var es6ClassVisitors = require('jstransform/visitors/es6-class-visitors').visitorList;
+var es6ArrowVisitors = require('jstransform/visitors/es6-arrow-function-visitors').visitorList;
 
-var visitors = es6ClassVisitors;
+var visitors = es6ClassVisitors.concat(es6ArrowVisitors);
 
 function transform(source) {
   return jstransform.transform(
