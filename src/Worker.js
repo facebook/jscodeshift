6fcdb03696574c65377f3b612efc908c401c3feb
Brie <breanna.bunge@airbnb.com>
2018-11-18 01:38:43 -0800

    Support for transpiling TS transform files

--- src/Worker.js
+++ src/Worker.js
@@ -15,6 +15,7 @@ const EventEmitter = require('events').EventEmitter;
 const async = require('neo-async');
 const fs = require('fs');
 const writeFileAtomic = require('write-file-atomic');
+const { DEFAULT_EXTENSIONS } = require('@babel/core');
 const getParser = require('./getParser');
 
 const jscodeshift = require('./core');
@@ -58,7 +59,15 @@ function setup(tr, babel) {
       presets: [
         '@babel/preset-env',
         '@babel/preset-flow',
+        '@babel/preset-typescript',
       ],
+      plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread'
+      ],
+      extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+      // By default, babel register only compiles things inside the current working directory.
+      // https://github.com/babel/babel/blob/2a4f16236656178e84b05b8915aab9261c55782c/packages/babel-register/src/node.js#L140-L157
       ignore: []
     });
   }
