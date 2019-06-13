e33435c7067cc068ceec9dadb34d5cf718763c4a
mikob <fxrock2002@yahoo.com>
2019-06-13 15:52:59 +0300

    async support for transforms

--- src/Worker.js
+++ src/Worker.js
@@ -135,7 +135,7 @@ function run(data) {
   async.each(
     files,
     function(file, callback) {
-      fs.readFile(file, function(err, source) {
+      fs.readFile(file, async function(err, source) {
         if (err) {
           updateStatus('error', file, 'File error: ' + err);
           callback();
@@ -144,7 +144,7 @@ function run(data) {
         source = source.toString();
         try {
           const jscodeshift = prepareJscodeshift(options);
-          const out = transform(
+          const out = await transform(
             {
               path: file,
               source: source,
