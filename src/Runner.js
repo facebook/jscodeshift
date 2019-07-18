298690d1bed85ecb0cb156d158fe19bb9a439803
Sunil Pai <threepointone@fb.com>
2019-07-18 19:21:17 +0100

    Fix Unhandled Promise rejection when called with a non-existent path
    When jscodeshift is called with a path that doesn't exist, `concatAll()` gets called with `undefined`, which isn't an interable, and throws an error. This PR fixes the promise being resolved by matching it with the ignore case.

--- src/Runner.js
+++ src/Runner.js
@@ -141,7 +141,7 @@ function getAllFiles(paths, filter) {
       fs.lstat(file, (err, stat) => {
         if (err) {
           process.stderr.write('Skipping path ' + file + ' which does not exist. \n');
-          resolve();
+          resolve([]);
           return;
         }
 
