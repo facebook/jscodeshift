928fd90ee6e15d207a9d8c1fb7652efbbbb5f643
Rob Yoder <rob@agilebits.com>
2021-04-30 16:07:31 -0600

    Remove lodash dependency entirely

--- parser/tsx.js
+++ parser/tsx.js
@@ -8,11 +8,9 @@
 
 'use strict';
 
-const _ = require('lodash');
 const babylon = require('@babel/parser');
-const baseOptions = require('./tsOptions');
-
-const options = _.merge(baseOptions, { plugins: ['jsx'] });
+const options = require('./tsOptions');
+options.plugins.push('jsx');
 
 /**
  * Doesn't accept custom options because babylon should be used directly in
