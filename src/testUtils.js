a548923ef8c1235086be13a414c6da4a3bd074ef
Chris Purssell <chris.purssell@workday.com>
2022-11-12 16:35:50 +1000

    fix: Use transform's exported parser in testUtils

--- src/testUtils.js
+++ src/testUtils.js
@@ -85,7 +85,9 @@ function runTest(dirName, transformName, options, testFilePrefix, testOptions = 
     testFilePrefix = transformName;
   }
 
-  const extension = extensionForParser(testOptions.parser)
+  // Assumes transform is one level up from __tests__ directory
+  const module = require(path.join(dirName, '..', transformName));
+  const extension = extensionForParser(testOptions.parser || module.parser)
   const fixtureDir = path.join(dirName, '..', '__testfixtures__');
   const inputPath = path.join(fixtureDir, testFilePrefix + `.input.${extension}`);
   const source = fs.readFileSync(inputPath, 'utf8');
@@ -93,8 +95,6 @@ function runTest(dirName, transformName, options, testFilePrefix, testOptions = 
     path.join(fixtureDir, testFilePrefix + `.output.${extension}`),
     'utf8'
   );
-  // Assumes transform is one level up from __tests__ directory
-  const module = require(path.join(dirName, '..', transformName));
   runInlineTest(module, options, {
     path: inputPath,
     source
@@ -140,7 +140,7 @@ exports.defineSnapshotTest = defineSnapshotTest;
  * Handles file-loading boilerplates, using same defaults as defineTest
  */
 function defineSnapshotTestFromFixture(dirName, module, options, testFilePrefix, testName, testOptions = {}) {
-  const extension = extensionForParser(testOptions.parser)
+  const extension = extensionForParser(testOptions.parser || module.parser)
   const fixtureDir = path.join(dirName, '..', '__testfixtures__');
   const inputPath = path.join(fixtureDir, testFilePrefix + `.input.${extension}`);
   const source = fs.readFileSync(inputPath, 'utf8');
