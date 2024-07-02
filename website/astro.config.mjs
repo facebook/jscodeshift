dc7c7194422ae5926fe26e563866cbae1a1eda01
Mohab Sameh <mohabsameh@outlook.com>
2024-07-02 16:22:28 +0300

    update social link

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
 		starlight({
 			title: 'jscodeshift',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/facebook/jscodeshift',
 			},
 			sidebar: [
 				{
