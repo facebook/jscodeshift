a67243e733992ca5a3744c433c8a8440bd492a86
ed-thuando <thuando.work@gmail.com>
2026-04-11 17:22:47 +0700

    fix: resolve #464 — Removing TSPropertySignature node adds commas to the type declaration
    Fixes #464
    
    Signed-off-by: ed-thuando <231172918+ed-thuando@users.noreply.github.com>

--- parser/ts.js
+++ parser/ts.js
@@ -9,7 +9,15 @@
 'use strict';
 
 const babylon = require('@babel/parser');
-const options = require('./tsOptions');
+const baseOptions = require('./tsOptions');
+const options = {
+  ...baseOptions,
+  plugins: baseOptions.plugins.map(plugin =>
+    Array.isArray(plugin) && plugin[0] === 'typescript'
+      ? ['typescript', { disallowAmbiguousJSXLikeChildren: true }]
+      : plugin
+  ),
+};
 
 /**
  * Doesn't accept custom options because babylon should be used directly in
