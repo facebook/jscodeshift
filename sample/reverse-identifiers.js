7e838094e461c79dea690ed73bc086b061693800
ed-thuando <thuando.work@gmail.com>
2026-04-11 17:31:35 +0700

    fix: resolve #193 — Missing simple examples for new users
    Fixes #193
    
    Signed-off-by: ed-thuando <231172918+ed-thuando@users.noreply.github.com>

--- sample/reverse-identifiers.js
+++ sample/reverse-identifiers.js
@@ -5,19 +5,32 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+import type {
+  ASTPath,
+  Collection,
+  JSCodeshift,
+  Transform,
+} from 'jscodeshift';
+
 /**
  * Example jscodeshift transformer. Simply reverses the names of all
  * identifiers.
  */
-function transformer(file, api) {
+const transform: Transform = (file, api: JSCodeshift) => {
   const j = api.jscodeshift;
 
-  return j(file.source)
-    .find(j.Identifier)
-    .replaceWith(
-      p => j.identifier(p.node.name.split('').reverse().join(''))
-    )
-    .toSource();
-}
+  const buildTransformer = () => {
+    return (path: ASTPath) => {
+      const reversed = path.node.name.split('').reverse().join('');
+      path.node.name = reversed;
+    };
+  };
 
-module.exports = transformer;
+  const root: Collection = j(file.source);
+
+  root.find(j.Identifier).forEach(buildTransformer());
+
+  return root.toSource();
+};
+
+export default transform;
